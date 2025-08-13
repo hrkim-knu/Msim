674571802ffc5466bf9ac52310d140145e0819ba
hrkim-knu <hn02301@gmail.com>
2025-08-13 19:28:53 +0900

    latest version

--- MasterSim/src/MSIM_AlgorithmGaussJacobi.cpp
+++ MasterSim/src/MSIM_AlgorithmGaussJacobi.cpp
@@ -5,9 +5,7 @@
 #include "MSIM_MasterSim.h"
 
 // hrkim 
-// #include <omp.h>
 #include <thread>
-// #include <vector>
 
 #include <IBK_messages.h>
 
@@ -45,21 +43,12 @@ AbstractAlgorithm::Result AlgorithmGaussJacobi::doStep() {
 			++m_nIterations; // add one iteration
 			
 			
-			// hrkim
-			// double maxSlaveTime = 0.0;
-			// double parallel_start = omp_get_wtime();
-			
-			// hrkim end
-			// loop over all slaves
-			// #pragma omp parallel for //schedule(dynamic,1)
-			
 			// hrkim : std::vector to hold threads for this cycle
-			// std::vector<std::thread> threads;
+			std::vector<std::thread> threads;
 
 			for (unsigned int s=0; s<cycle.m_slaves.size(); ++s) {
 				// hrkim
-				// double slave_start = omp_get_wtime();
-				// threads.push_back(std::thread([&, s]() {
+				threads.push_back(std::thread([&, s]() {
 
 
 				AbstractSlave * slave = cycle.m_slaves[s];
@@ -91,36 +80,13 @@ AbstractAlgorithm::Result AlgorithmGaussJacobi::doStep() {
 				// sync results into vector with newly computed quantities
 				m_master->syncSlaveOutputs(slave, m_master->m_realytNext, m_master->m_intytNext, m_master->m_boolytNext, m_master->m_stringytNext, false);
 				
-				// hrkim
-				// double slave_time = omp_get_wtime() - slave_start;
-				// maxSlaveTime = (slave_time > maxSlaveTime) ? slave_time : maxSlaveTime;
-				// })); // hrkim : end of thread
+				})); // hrkim : end of thread
 			}
 			// hrkim : thread join : wait until the end of all slave's doStep()
-			// for (auto & t : threads) {
-			// 	if (t.joinable())
-			// 		t.join();
-			// }
-			// hrkim 
-			// double parallel_end = omp_get_wtime();
-			// double overallParallelTime = parallel_end - parallel_start;
-			
-			// double overheadTime = overallParallelTime - maxSlaveTime;
-			
-			// accumulatedOverhead += overheadTime;
-			// accumulatedParallelTime += overallParallelTime;
-			
-			// if (cnt == 9997)
-			// {ECU/script/EXP2$ ./exp2_ECU3.sh
-
-				// std::cout << cnt << " : 전체 accumulated 병렬 실행 시간 = " << accumulatedParallelTime 
-				// << " sec, 최대 doStep() 실행 시간 = " << maxSlaveTime 
-				// << " sec, 추정 오버헤드 = " << overheadTime << " sec\n"
-				// << "누적 오버헤드 = " << accumulatedOverhead << " sec\n";
-				// }
-				// cnt++;
-				// hrkim end
-			// }
+			for (auto & t : threads) {
+				if (t.joinable())
+					t.join();
+			}
 			
 			// ** algorithm end **
 			
